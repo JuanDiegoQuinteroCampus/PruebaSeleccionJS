681f40ff9612761414ccf829bf967c51b25a46ae
JuanDiegoQuinteroCampus <juanquinterocampus33@gmail.com>
2023-05-08 12:58:44 -0500

    feat: :sparkles: TableFinish
    Se imprimen los dato del formulario en la tabla correctamente, adicional se le puso el title

--- main.js
+++ main.js
@@ -1,42 +1,71 @@
 const formulario = document.getElementById("formulario");
-formulario.addEventListener("submit", agregarDato);
+
 function agregarDato(event) {
     event.preventDefault(); // Previene que se recargue la página
     
     // Obtiene los valores ingresados ​​en el formulario
-    const nombre = document.getElementById("nombre").value;
-    const edad = document.getElementById("edad").value;
-    const telefono = document.getElementById("telefono").value;
-    const email = document.getElementById("email").value;
-    const direccion = document.getElementById("direccion").value;
-    const born = document.getElementById("born").value;
-    const NumIdentificacion = document.getElementById("NumIdentificacion").value;
-    const Ingreso = document.getElementById("Ingreso").value;
-    const idTeam = document.getElementById("id_team").value;
     
     
-    /* // Crea una nueva fila en la tabla con los datos ingresados
+    // Crea una nueva fila en la tabla con los datos ingresados
     const nuevaFila = `
       <tr>
         <td>
           <p>${nombre} </p>
         </td>
+        <td>
+          <p>${edad} </p>
+        </td>
+        <td>
+          <p>${telefono} </p>
+        </td>
+        <td>
+          <p>${email} </p>
+        </td>
+        <td>
+          <p>${direccion} </p>
+        </td>
+        <td>
+          <p>${born} </p>
+        </td>
+        <td>
+          <p>${NumIdentificacion} </p>
+        </td>
+        <td>
+          <p>${Ingreso} </p>
+        </td>
+        <td>
+          <p>${idTeam} </p>
+        </td>
       </tr>
     `;
 
-    if (option === 'Ingresos') {
+    
         const tablaDatos = document.getElementById("tabla-datos");
         tablaDatos.insertAdjacentHTML("beforeend", nuevaFila);
 
-   */
-
-        
-    // Limpia los valores del formulario
-    document.getElementById("selectOption").value = "";
-    document.getElementById("desc").value = "";
-    document.getElementById("userInput").value = "";
-  }
+  
+}
   
   
+  function guardar(){
+   
+    let nombre = document.getElementById("nombre").value;
+    let edad = document.getElementById("edad").value;
+    let telefono = document.getElementById("telefono").value;
+    let email = document.getElementById("email").value;
+    let direccion = document.getElementById("direccion").value;
+    let born = document.getElementById("born").value;
+    let NumIdentificacion = document.getElementById("NumIdentificacion").value;
+    let Ingreso = document.getElementById("Ingreso").value;
+    let idTeam = document.getElementById("id_team").value;
+    
 
+    
+
+    let fila="<tr><td>"+nombre+"</td><td>"+edad +"</td><td>"+telefono +"</td><td>"+email+"</td><td>"+direccion +"</td><td>"+born +"</td><td>"+NumIdentificacion+"</td><td>"+Ingreso +"</td><td>"+idTeam +"</td></tr>";
+
+    let btn = document.createElement("TR");
+   	btn.innerHTML=fila;
+    document.getElementById("tablita").appendChild(btn);
+}
 
